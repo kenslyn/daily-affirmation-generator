64e1189bef11d1991c9ee9a647980194f0b0ef4e
Mackenzie Agnir <121129438+kenslyn@users.noreply.github.com>
2024-02-27 14:39:17 -0700

    Updated typewriter delay and CSS effect

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayAffirmation(response) {
   new Typewriter("#affirmation", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 20,
+    delay: 35,
     cursor: "",
   });
 }
@@ -13,13 +13,13 @@ function generateAffirmation(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "bete3d4bc84ffa94be264oaebfb7012c";
   let context =
-    "You are a positive affirmation expert and love to write short yet meaningful affirmations. Your mission is to generate 1 line affirmation in basic HTML format with quotation marks at the beginning and end. Make sure to follow the user instructions.";
+    "You are a positive affirmation expert and love to write short yet meaningful affirmations. Your mission is to generate 1 line affirmation in basic HTML format with quotation marks at the beginning and end of the line. Make sure to follow the user instructions.";
   let prompt = `User instructions: Generate a positive affirmation about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let affirmationElement = document.querySelector("#affirmation");
   affirmationElement.classList.remove("hidden");
-  affirmationElement.innerHTML = `<div class="generating">⏳ Generating your Daily Affirmation about ${instructionsInput.value}</div>`;
+  affirmationElement.innerHTML = `<div class="blink">⏳ Generating your Daily Affirmation about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayAffirmation);
 }
