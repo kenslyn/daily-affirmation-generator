0dbc866862a12eb33c01c9f743e093429a52e191
Mackenzie Agnir <121129438+kenslyn@users.noreply.github.com>
2024-02-27 12:01:40 -0700

    Added the basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateAffirmation(event) {
+  event.preventDefault();
 
+  new Typewriter("#affirmation", {
+    strings: ["I love myself just as I am today."],
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
+
+let affirmationFormElement = document.querySelector(
+  "#affirmation-generator-form"
+);
+affirmationFormElement.addEventListener("submit", generateAffirmation);
