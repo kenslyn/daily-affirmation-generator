83d69cffcb1be13dbe0249e3923a126e6db7ef9c
Mackenzie Agnir <121129438+kenslyn@users.noreply.github.com>
2024-02-27 14:47:47 -0700

    Updated AI context prompt

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateAffirmation(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "bete3d4bc84ffa94be264oaebfb7012c";
   let context =
-    "You are a positive affirmation expert and love to write short yet meaningful affirmations. Your mission is to generate 1 line affirmation in basic HTML format with quotation marks at the beginning and end of the line. Make sure to follow the user instructions.";
+    "You are a positive affirmation expert and love to write short yet meaningful affirmations. Your mission is to generate 1 line affirmation in basic HTML format with quotation marks at the beginning and end of the affirmation. No extra spaces. Make sure to follow the user instructions.";
   let prompt = `User instructions: Generate a positive affirmation about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
